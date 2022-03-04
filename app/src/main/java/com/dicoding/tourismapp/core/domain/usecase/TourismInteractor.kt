f7b7ea09da724e68c4e7c598113532d2f44b0e53
muammarahlnn <21muammarahlan@gmail.com>
2022-03-04 19:51:07 +0800

    change reactive programming implementation change implementation from rxjava to coroutine flow

--- app/src/main/java/com/dicoding/tourismapp/core/domain/usecase/TourismInteractor.kt
+++ app/src/main/java/com/dicoding/tourismapp/core/domain/usecase/TourismInteractor.kt
@@ -3,16 +3,16 @@ package com.dicoding.tourismapp.core.domain.usecase
 import com.dicoding.tourismapp.core.data.Resource
 import com.dicoding.tourismapp.core.domain.model.Tourism
 import com.dicoding.tourismapp.core.domain.repository.ITourismRepository
-import io.reactivex.Flowable
+import kotlinx.coroutines.flow.Flow
 
 class TourismInteractor(
     private val tourismRepository: ITourismRepository
 ) : TourismUseCase{
 
-    override fun getAllTourism(): Flowable<Resource<List<Tourism>>> =
+    override fun getAllTourism(): Flow<Resource<List<Tourism>>> =
         tourismRepository.getAllTourism()
 
-    override fun getFavoriteTourism(): Flowable<List<Tourism>> =
+    override fun getFavoriteTourism(): Flow<List<Tourism>> =
         tourismRepository.getFavoriteTourism()
 
     override fun setFavoriteTourism(tourism: Tourism, state: Boolean) =
