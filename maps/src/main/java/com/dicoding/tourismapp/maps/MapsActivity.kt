76add6b4d899ed0b2a251a6907c9e0640ba27a4e
muammarahlnn <21muammarahlan@gmail.com>
2022-03-26 18:43:40 +0800

    show every tourism place location (still error)
    show every tourism place location in a map using mapbox

--- maps/src/main/java/com/dicoding/tourismapp/maps/MapsActivity.kt
+++ maps/src/main/java/com/dicoding/tourismapp/maps/MapsActivity.kt
@@ -1,13 +1,26 @@
 package com.dicoding.tourismapp.maps
 
+import android.content.Intent
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.activity.viewModels
 import com.dicoding.tourismapp.core.data.Resource
+import com.dicoding.tourismapp.core.domain.model.Tourism
 import com.dicoding.tourismapp.databinding.ActivityMainBinding
+import com.dicoding.tourismapp.detail.DetailTourismActivity
 import com.dicoding.tourismapp.di.MapsModuleDependencies
 import com.dicoding.tourismapp.maps.databinding.ActivityMapsBinding
+import com.google.gson.Gson
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
+import com.mapbox.mapboxsdk.geometry.LatLng
+import com.mapbox.mapboxsdk.geometry.LatLngBounds
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.maps.Style
+import com.mapbox.mapboxsdk.plugins.annotation.SymbolManager
+import com.mapbox.mapboxsdk.plugins.annotation.SymbolOptions
 import dagger.hilt.android.EntryPointAccessors
 import javax.inject.Inject
 
@@ -15,6 +28,8 @@ class MapsActivity : AppCompatActivity() {
 
     @Inject
     lateinit var factory: ViewModelFactory
+
+    private lateinit var mapboxMap: MapboxMap
 
     private val mapsViewModel: MapsViewModel by viewModels {
         factory
@@ -35,12 +50,53 @@ class MapsActivity : AppCompatActivity() {
             .inject(this)
 
         super.onCreate(savedInstanceState)
+
+        Mapbox.getInstance(this, getString(R.string.mapbox_access_token))
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         supportActionBar?.title = "Tourism Map"
 
-        getTourismData()
+        binding.mapView.onCreate(savedInstanceState)
+        binding.mapView.getMapAsync { mapboxMap ->
+            this.mapboxMap = mapboxMap
+            getTourismData()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.mapView.onStop()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView.onLowMemory()
     }
 
     private fun getTourismData() {
@@ -50,7 +106,7 @@ class MapsActivity : AppCompatActivity() {
                     is Resource.Loading -> binding.progressBar.visibility = View.VISIBLE
                     is Resource.Success -> {
                         binding.progressBar.visibility = View.GONE
-                        binding.tvMaps.text = "This is map of ${tourism.data?.get(0)?.name}"
+                        showMarker(tourism.data)
                     }
                     is Resource.Error -> {
                         binding.progressBar.visibility = View.GONE
@@ -60,5 +116,41 @@ class MapsActivity : AppCompatActivity() {
                 }
             }
         })
+    }
+
+    private fun showMarker(dataTourism: List<Tourism>?) {
+        mapboxMap.setStyle(Style.MAPBOX_STREETS) { style ->
+            style.addImage(ICON_ID, BitmapFactory.decodeResource(resources, R.drawable.mapbox_marker_icon_default))
+            val latLngBoundsBuilder = LatLngBounds.Builder()
+
+            val symbolManager = SymbolManager(binding.mapView, mapboxMap, style)
+            symbolManager.iconAllowOverlap = true
+
+            val options = ArrayList<SymbolOptions>()
+            dataTourism?.forEach { data ->
+                latLngBoundsBuilder.include(LatLng(data.latitude, data.longitude))
+                options.add(
+                    SymbolOptions()
+                        .withLatLng(LatLng(data.latitude, data.longitude))
+                        .withIconImage(ICON_ID)
+                        .withData(Gson().toJsonTree(data))
+                )
+            }
+            symbolManager.create(options)
+
+            val latLngBounds = latLngBoundsBuilder.build()
+            mapboxMap.easeCamera(CameraUpdateFactory.newLatLngBounds(latLngBounds, 50), 5000)
+
+            symbolManager.addClickListener { symbol ->
+                val data = Gson().fromJson(symbol.data, Tourism::class.java)
+                val intent = Intent(this, DetailTourismActivity::class.java)
+                intent.putExtra(DetailTourismActivity.EXTRA_DATA, data)
+                startActivity(intent)
+            }
+        }
+    }
+
+    companion object {
+        private const val ICON_ID = "ICON_ID"
     }
 }
